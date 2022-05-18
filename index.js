12828ff33ea0de44dc3da54eb17a6858635c35c1
Allie LeFever <alefever39@gmail.com>
2022-05-18 10:19:09 -0600

    commit tree 575531ea014cf47ec751ac7da84207e634c35142

--- index.js
+++ index.js
@@ -12,5 +12,14 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  const titleCasedTutorials = tutorials.map(item => {
+    const eachWord = item.split(' ')
+    const uppercasedWords = eachWord.map(item => item.charAt(0).toUpperCase() + item.slice(1))
+    const newStr = uppercasedWords.join(' ')
+    return newStr
+  })
+  return titleCasedTutorials
 }
+
+
+// console.log('what does the this keyword mean?'.split(' '));
